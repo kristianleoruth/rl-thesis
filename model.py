fa2abd97c7c5429e723095f702bb5ce0722fae58
kristianleoruth <kristianleoruth@gmail.com>
2025-03-26 13:04:53 +0100

    Initial commit

---
+++ model.py
@@ -0,0 +1,255 @@
+import argparse
+import gymnasium as gym
+from gymnasium import ObservationWrapper
+import stable_baselines3 as sb3
+import sb3_contrib as sb3c
+from stable_baselines3.common.env_util import make_atari_env, make_vec_env
+from stable_baselines3.common.atari_wrappers import AtariWrapper
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize, VecFrameStack
+import ale_py
+import random
+import torch
+import numpy as np
+import multiprocessing as mp
+import os
+from typing import List, Optional, Union
+os.environ["ALE_DISABLE_LOG"] = "1"
+
+ASTEROIDS_ENVID = "ALE/Asteroids-v5"
+# gym.register(ASTEROIDS_ENVID, )
+# gym.register_envs(ale_py)
+
+def linear_schedule(initial_lr: float, final_lr: float = 1e-5):
+    def func(progress_remaining: float) -> float:
+        return final_lr + (initial_lr - final_lr) * progress_remaining
+    return func
+
+
+def parse_mdl_args(argstr=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo", type=str, required=True, 
+                        help="Algorithm (ppo, rppo, trpo, a2c)")
+    parser.add_argument("--fc1", type=int, default=256, required=False,
+                        help="Size of first fully connected layer")
+    parser.add_argument("--fc2", type=int, default=128, required=False,
+                        help="Size of second fully connected layer")
+    parser.add_argument("--cnn", action="store_true", required=False,
+                        help="Use convolutional (Cnn) policy")
+    parser.add_argument("--lrsched", action="store_true", required=False,
+                        help="Use scheduled learning rate")
+    parser.add_argument("--lr", type=float, default=3e-4, required=False,
+                        help="Define learning rate")
+    parser.add_argument("--lrstart", "--lrs", type=float, default=3e-4, required=False,
+                        help="Starting LR (LR schedule)")
+    parser.add_argument("--lrend", "--lre", type=float, default=3e-4, required=False,
+                        help="Ending LR (LR schedule)")
+    parser.add_argument("--entcoef", "--entr", type=float, default=0.0, required=False,
+                        help="Entropy coefficienct (used if available for model)")
+    parser.add_argument("--kl", "--usekl", action="store_true", required=False,
+                        help="Use KL target (PPO, RPPO)")
+    parser.add_argument("--kltarg", "--klt", type=float, default=0.02, required=False,
+                        help="KL target/constraint (PPO, RPPO, TRPO)")
+    parser.add_argument("--clip", "--cliprange", type=float, default=0.2, required=False,
+                        help="Clip range (PPO, RPPO)")
+    parser.add_argument("--vfcoef", type=float, default=0.5, required=False,
+                        help="Value function loss coefficient")
+    parser.add_argument("--gae", type=float, default=0.95, required=False,
+                        help="GAE lambda")
+    parser.add_argument("--gamma", type=float, default=0.99, required=False,
+                        help="Gamma value")
+    parser.add_argument("--n_steps", type=int, default=4096, required=False,
+                        help="n_steps argument for model")
+    parser.add_argument("--batch_size", type=int, default=4096, required=False,
+                        help="batch size argument for model")
+    parser.add_argument("--n_envs", type=int, default=16, required=False,
+                        help="N envs per rollout")
+    parser.add_argument("--n_epochs", type=int, default=10, required=False,
+                        help="n_epochs argument to model")
+    parser.add_argument("--logdir", type=str, default="",
+                        help="Tensorboard log directory")
+    return parser.parse_args(argstr.split() if argstr is not None else None)
+
+
+class ScaleObsTo01(ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = gym.spaces.Box(
+            low=0.0, high=1.0, shape=env.observation_space.shape, dtype=np.float32)
+    
+    def observation(self, obs):
+        obs = np.clip(obs.astype(np.float32) / 255.0, 0.0, 1.0)
+        return np.transpose(obs, (2, 0, 1))
+
+
+class FixedSeedEnv(gym.Wrapper):
+    """
+        First reset according to specified seed,
+        further resets are stochastic
+    """
+    def __init__(self, env, seed):
+        super().__init__(env)
+        self._seed = seed
+        self._first_rst = True
+
+    def reset(self, **kwargs):
+        if self._first_rst:
+            if "seed" not in kwargs:
+                kwargs["seed"] = self._seed
+            self._first_rst = False
+        return self.env.reset(**kwargs)
+
+
+def get_callable_env(env_id: str, seed: Optional[int]):
+    def _func():
+        import gymnasium as gym  # re-import in subprocess
+        env = gym.make(env_id)   # this triggers ALE registration internally
+        from stable_baselines3.common.atari_wrappers import AtariWrapper
+        env = AtariWrapper(env)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        env = FixedSeedEnv(env, seed)
+        return env
+    return _func
+
+
+def get_env(env_id: str, n_envs: int=1, seed: int=None, n_stack: int=1):
+    """
+        Get AtariWrapper, VecEnv, (optional) VecFrameStack of env_name
+        Arguments:
+            env_id: corresponds to env_id in gym.make
+            n_envs: number of environments in parallel (for vec_env)
+            seed: None -> random, or int seed
+            n_stack: Argument to VecFrameStack i.e. how many frames to stack in obs
+        Returns:
+            tuple: (atari wrapped vec env, seed used)
+    """
+    if seed is None:
+        seed = random.randint(0, 0xefffffff)
+    env_fns = [get_callable_env(env_id, seed=seed+i) for i in range(n_envs)]
+    env = SubprocVecEnv(env_fns)
+    # env = VecNormalize(env, norm_obs=norm_obs, norm_reward=norm_reward)
+    if n_stack > 1:
+        env = VecFrameStack(env, n_stack=n_stack)
+    print("Obs shape:", env.observation_space.shape)
+    return env, seed
+
+
+def get_cnn_env(env_id, n_envs, seed=None):
+    """
+        Calls get_env with n_stack=4
+    """
+    return get_env(env_id, n_envs, seed, n_stack=4)
+
+
+def get_mlp_env(env_id, n_envs, seed=None):
+    # wrap with MLPWrapper
+    return
+
+
+def _get_policy(args):
+    policy = "CnnPolicy" if args.cnn else "MlpPolicy"
+    if args.algo == "rppo":
+        policy = "CnnLstmPolicy" if args.cnn else "MlpLstmPolicy"
+    return policy
+
+
+def _get_dev(args):
+    return "cuda" if args.cnn and torch.cuda.is_available() else "cpu"
+
+
+def fix_mp_macos():
+    mp.set_start_method("fork", force=True)
+
+
+def get_model(argstr, env, seed=None):
+    """
+        Get PPO, TRPO, A2C models based on argstr, pass -h for list of args
+
+        Returns (model, seed)
+    """
+    args = parse_mdl_args(argstr)
+    args.algo = args.algo.lower()
+    if seed is None:
+        seed = random.randint(0, 0xefffffff)
+    mdl = None
+    _dict = None
+    match args.algo:
+        case "ppo":
+            _dict = _get_ppo(args, env, seed)
+            mdl = sb3.PPO(**_dict)
+        case "trpo":
+            _dict = _get_trpo(args, env, seed)
+            mdl = sb3c.TRPO(**_dict)
+    print(_dict)
+    return mdl, seed
+    
+
+def _get_ppo(args, env, seed):
+    lr = linear_schedule(args.lrstart, args.lrend) if args.lrsched else args.lr
+    policy_kwargs = dict(
+        net_arch=dict(
+            pi=[args.fc1, args.fc2],
+            vf=[args.fc1, args.fc2]
+        ),
+        normalize_images=args.cnn,
+    )
+
+    mdl_dict = dict(
+        policy=_get_policy(args),
+        device=_get_dev(args),
+        env=env,
+        learning_rate=lr,
+        seed=seed,
+        policy_kwargs=policy_kwargs,
+        verbose=1,
+        gamma=args.gamma,
+        gae_lambda=args.gae,
+        normalize_advantage=True,
+        n_steps=args.n_steps,
+        batch_size=args.batch_size,
+        n_epochs=args.n_epochs,
+        ent_coef=args.entcoef,
+        clip_range=args.clip,
+        vf_coef=args.vfcoef,
+    )
+
+    if args.kl:
+        mdl_dict["target_kl"] = args.kltarg
+
+    if args.logdir != "":
+        mdl_dict["tensorboard_log"] = args.logdir
+    return mdl_dict
+
+
+def _get_trpo(args, env, seed):
+    lr = linear_schedule(args.lrstart, args.lrend) if args.lrsched else args.lr
+    policy_kwargs = dict(
+        net_arch=[args.fc1, args.fc2],
+        normalize_images=args.cnn,
+    )
+
+    mdl_dict = dict(
+        policy=_get_policy(args),
+        device=_get_dev(args),
+        env=env,
+        seed=seed,
+        learning_rate=lr,
+        policy_kwargs=policy_kwargs,
+        n_steps=args.n_steps,
+        batch_size=args.batch_size,
+        gamma=args.gamma,
+        gae_lambda=args.gae,
+        normalize_advantage=True,
+        target_kl=args.kltarg,
+        verbose=1,
+    )
+
+    if args.logdir != "":
+        mdl_dict["tensorboard_log"] = args.logdir
+
+    return mdl_dict
+
+
+if __name__ == "__main__":
+    env, _ = get_cnn_env(ASTEROIDS_ENVID, 4, 1234)
+    mdl, _ = get_model("--algo ppo --cnn --n_steps 1024 --batch_size 1024", env, 1234)
+    mdl.learn(1e6)
---
+++ tune.py
@@ -0,0 +1,164 @@
+import model
+import optuna
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import BaseCallback
+import argparse
+import gc
+
+
+class TrialEvalCallback(BaseCallback):
+    def __init__(self, trial, eval_env, prune_after=500_000, eval_freq=15000, n_eval_episodes=25, verbose=0):
+        super().__init__(verbose)
+        self.trial = trial
+        self.eval_env = eval_env
+        self.eval_freq = eval_freq
+        self.n_eval_episodes = n_eval_episodes
+        self.step = 0
+        self.prune_after = prune_after
+
+    def _on_step(self):
+        self.step += 1
+        if self.step % self.eval_freq != 0 or self.prune_after > self.step:
+            return True
+
+        mean_reward, _ = evaluate_policy(
+            self.model, self.eval_env, n_eval_episodes=self.n_eval_episodes, return_episode_rewards=False
+        )
+
+        self.trial.report(mean_reward, self.step)
+
+        if self.trial.should_prune():
+            raise optuna.exceptions.TrialPruned()
+
+        return True
+
+
+def build_argstr(trial: optuna.Trial, n_envs: int, algo: str, **kwargs):
+    """
+        kwargs:
+            cnn=True, passes --cnn flag
+            for ppo, can pass kl=True to tune kl target
+    """
+    cmd = f"--algo {algo} --fc1 1024 --fc2 512"
+    cnn = "cnn" in kwargs.keys() and kwargs["cnn"]
+    lr = trial.suggest_float("lr", 1e-5 if cnn else 1e-4, 3e-4 if cnn else 3e-3, step=1e-5)
+    gae_lambda = trial.suggest_float("gae_lambda", 0.9, 0.99, step=0.01)
+    gamma = trial.suggest_float("gamma", 0.9, 0.99, step=0.01)
+    cmd += f" --lr {lr} --gae {gae_lambda} --gamma {gamma}"
+
+    if cnn:
+        cmd += " --cnn"
+    match algo:
+        case "ppo":
+            vf_coef = trial.suggest_float("vf_coef", 0.2, 0.7, step=0.1)
+            ent_coef = trial.suggest_float("ent_coef", 0.005, 0.02, step=0.001)
+            n_steps = trial.suggest_categorical("n_steps", [2048,4096,8192])
+            batch_size = trial.suggest_categorical("batch_size", [1024,2048,4096,8192])
+            if batch_size > n_steps * n_envs:
+                raise optuna.exceptions.TrialPruned()
+            n_epochs = trial.suggest_int("n_epochs", 5, 25, step=5)
+            cmd += f" --vfcoef {vf_coef} --entcoef {ent_coef} --n_steps {n_steps}"
+            cmd += f" --batch_size {batch_size} --n_epochs {n_epochs}"
+            if "kl" in kwargs.keys() and kwargs["kl"]:
+                kl_target = trial.suggest_float("kl_target", 0.01, 0.1, step=0.01)
+                cmd += f" --kl --kltarg {kl_target}"
+            else:
+                clip_range = trial.suggest_float("clip_range", 0.05, 0.2, step=0.01)
+                cmd += f" --clip {clip_range}"
+    return cmd
+
+
+def objective(trial: optuna.Trial, algo: str, env_id: str,
+              n_envs: int=1, seed: int=None, **kwargs):
+    """
+        args:
+            algo=ppo, trpo, a2c
+            seed=seed to pass to model creator, env creator
+            n_envs=number of parallelized envs to create
+            env_id=env id
+        kwargs:
+            cnn=True, passes --cnn flag
+            for ppo, can pass kl=True to tune kl target
+            learn_ts=int, learning timesteps
+            prune_after=int, start pruning after N steps
+    """
+    model_str = build_argstr(trial, n_envs, algo, **kwargs)
+    env = None
+    eval_env = None
+    if "cnn" in kwargs.keys() and kwargs["cnn"]:
+        env, _ = model.get_cnn_env(env_id, n_envs, seed)
+        eval_env, _ = model.get_cnn_env(env_id, 1, seed)
+    else:
+        # get_mlp_env
+        pass
+
+    if "learn_ts" not in kwargs.keys():
+        kwargs["learn_ts"] = 500_000
+    if "prune_after" not in kwargs.keys():
+        kwargs["prune_after"] = kwargs["learn_ts"]
+
+    mdl, _ = model.get_model(model_str, env, seed)
+
+    callback = TrialEvalCallback(trial, eval_env, prune_after=kwargs["prune_after"], eval_freq=20000, n_eval_episodes=25, verbose=1)
+    try:
+        mdl.learn(kwargs["learn_ts"], callback=callback)
+    except optuna.exceptions.TrialPruned:
+        raise
+    except Exception as e:
+        trial.set_user_attr("fail_reason", str(e))
+        raise
+
+    mean_ret, _ = evaluate_policy(mdl, eval_env, n_eval_episodes=50)
+    del mdl, eval_env, env
+    gc.collect()
+    return mean_ret
+
+
+def opt_hyperparams(algo: str, env_id: str, n_envs: int=1, seed: int=None, 
+                    study_name="tuning", store_dir: str="hyperparam_studies/",
+                    n_trials=50, **kwargs):
+    """
+        args:
+            algo=ppo, trpo, a2c
+            seed=seed to pass to model creator, env creator
+            n_envs=number of parallelized envs to create
+            env_id=env id
+            prune_after
+        kwargs:
+            cnn=True, passes --cnn flag
+            for ppo, can pass kl=True to tune kl target
+            learn_ts=int, learning timesteps
+            prune_after=int, start pruning after N steps
+    """
+    study = optuna.create_study(direction="maximize", storage=f"sqlite:///{store_dir}{study_name}.db",
+                                load_if_exists=True, study_name=study_name,
+                                pruner=optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=2))
+    study.optimize(lambda trial: objective(trial, algo, env_id, n_envs, seed, **kwargs),
+                   n_trials=n_trials)
+    return study
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo", type=str, required=True,
+                        help="Algorithm to tune parameters for")
+    parser.add_argument("--env_id", "--env", type=str, default=model.ASTEROIDS_ENVID,
+                        help="Env id to load")
+    parser.add_argument("--n_envs", type=int, default=8,
+                        help="Number of environments to train on")
+    parser.add_argument("--n_trials", type=int, default=50)
+    parser.add_argument("--cnn", action="store_true",
+                        help="Use convolutional policy")
+    parser.add_argument("--kl", action="store_true",
+                        help="Tune KL target instead of clip range (PPO)")
+    parser.add_argument("--learn_ts", type=int, default=int(1e6),
+                        help="Timesteps to learn per trial")
+    parser.add_argument("--study_name", "--name", type=str, default="tuning")
+    parser.add_argument("--store_dir", "--dir", type=str, default="hyperparam_studies/",
+                        help="(Relative path, do not use ./) directory to store study db, must end with /")
+    parser.add_argument("--seed", type=int, default=1, help="Seed (passed to model creator, env creator)")
+    parser.add_argument("--prune_after", type=int, default=500_000,
+                        help="Start checking pruning after N steps")
+    args = parser.parse_args()
+    opt_hyperparams(args.algo.lower(), args.env_id, args.n_envs, args.seed, args.study_name, args.store_dir,
+                    args.n_trials, cnn=args.cnn, kl=args.kl, learn_ts=args.learn_ts)
