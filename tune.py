8d907a8114283e44f276404c0964b980fc9b9c0f
Kristian Ruth <kristianleoruth@gmail.com>
2025-04-20 16:49:29 +0200

    Merge

--- tune.py
+++ tune.py
@@ -157,7 +157,7 @@ def build_argstr(trial: optuna.Trial, n_envs: int, algo: str, **kwargs):
 
             gamma = 0.99
             gae_lambda = 0.95
-            n_steps = 1024
+            n_steps = 2048
             batch_size = n_envs * n_steps
             cg_max_steps = trial.suggest_categorical("cg_max_steps", [10, 15, 20])
             cg_damping = trial.suggest_categorical("cg_damping", [0.05, 0.1, 0.2])
